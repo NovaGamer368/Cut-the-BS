7368f24f29745f105563663833f4f1bff8703f3d
Brennan Phifer <bman5803@gmail.com>
2023-02-06 18:47:10 -0700

    Add project files.

---
+++ Cut the BS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Cut_the_BS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
