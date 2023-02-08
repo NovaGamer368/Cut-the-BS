8a84da2d5fd999c9cd847929e06f3ae407a677ee
Brennan Phifer <bman5803@gmail.com>
2023-02-07 17:46:44 -0700

    Added the DAL

--- Cut the BS/Data/UserListDAL.cs
+++ Cut the BS/Data/UserListDAL.cs
@@ -1,6 +1,28 @@
-﻿namespace Cut_the_BS.Data
+﻿using Cut_the_BS.Models;
+using Cut_the_BS.Interfaces;
+
+namespace Cut_the_BS.Data
 {
-    public class UserListDAL
+    public class UserListDAL : IUserDataAccessLayer
     {
+        public void AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
