1d01d6da09da62fe3623035ad001a2ff4cb059fe
Brennan Phifer <bman5803@gmail.com>
2023-02-07 18:13:53 -0700

    Got the Database all working! No actual functions yet.

---
+++ Cut the BS/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Cut_the_BS.Models;
+
+namespace Cut_the_BS.Data
+{
+    
+
+    
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions options) : base(options)
+            {
+
+            }
+
+            public DbSet<User> Users { get; set; }
+        }
+    
+
+}
