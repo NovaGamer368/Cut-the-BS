60a03b0874d1f80d82d38e19a7a8722c6b356803
Brennan Phifer <bman5803@gmail.com>
2023-02-07 17:05:30 -0700

    Added User Model

---
+++ Cut the BS/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Cut_the_BS.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string? Name { get; set; }
+        public string? UserName { get; set; }
+        public string? Address { get; set; }
+    }
+}
