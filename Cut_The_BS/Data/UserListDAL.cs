3d25e3fa564dca51cef34e24e4ad84097f41334e
Harlea R Johnston <98293078+HarleaJohnston@users.noreply.github.com>
2023-02-13 17:41:38 -0700

    bye bye new project

---
+++ Cut_The_BS/Data/UserListDAL.cs
@@ -0,0 +1,34 @@
+﻿using Cut_the_BS.Models;
+using Cut_the_BS.Interfaces;
+
+namespace Cut_the_BS.Data
+{
+    public class UserListDAL : IUserDataAccessLayer
+    {
+        private AppDbContext db;
+        public UserListDAL(AppDbContext db)
+        {   
+            this.db = db;
+        }
+        public void AddUser(User user)
+        {
+            db.Users.Add(user);
+            db.SaveChanges();
+        }
+
+        public void EditUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User GetUser(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<User> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
