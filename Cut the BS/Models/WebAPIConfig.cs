7c3e5413ec5bf32f19d4084bbd65b833b4222157
Brennan Phifer <bman5803@gmail.com>
2023-02-21 17:58:30 -0700

    Revert "test"
    This reverts commit 24ec09900c7e2d495c3d9aa845a332a0193d7d15.

--- Cut the BS/Models/WebAPIConfig.cs
+++ Cut the BS/Models/WebAPIConfig.cs
@@ -11,6 +11,12 @@ namespace Cut_the_BS.Models
             // Web API routes
             config.MapHttpAttributeRoutes();
 
+            config.Routes.MapHttpRoute(
+                name: "RecipeApi",
+                routeTemplate: "www.themealdb.com/api/json/v1/1/search.php?s={searchKey}/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
             // WebAPI when dealing with JSON & JavaScript!
             // Setup json serialization to serialize classes to camel (std. Json format)
             var formatter = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
