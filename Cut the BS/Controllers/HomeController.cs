d414a38bf37ddbef33a6c695601ae45c8b0a0a9c
Harlea R Johnston <98293078+HarleaJohnston@users.noreply.github.com>
2023-02-27 13:17:34 -0700

    DID I FINALLY GET THE API TO CONNECT???? HELL YEAH I DID! WOOOOOOOOOOOOOOOOOOO

--- Cut the BS/Controllers/HomeController.cs
+++ Cut the BS/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using Cut_the_BS.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Data;
 using System.Diagnostics;
+using System.Net.Http.Headers;
 
 namespace Cut_the_BS.Controllers
 {
@@ -8,6 +11,8 @@ namespace Cut_the_BS.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        string baseURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+        //string baseURL = "https://randomuser.me/api/?results=5";
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -28,10 +33,31 @@ namespace Cut_the_BS.Controllers
             return View();
         }
 
-        public IActionResult API()
+        public async Task<IActionResult> API()
         {
+            //Calling API and populating it into a DataTable
+            DataTable dt = new DataTable();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(baseURL);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage getData = await client.GetAsync("");
+
+                if (getData.IsSuccessStatusCode)
+                {
+                    string result = getData.Content.ReadAsStringAsync().Result;
+                    dt = JsonConvert.DeserializeObject<DataTable>(result);
+                }
+                else
+                {
+                    Console.WriteLine("API said nah fam");
+                }
+            }
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
