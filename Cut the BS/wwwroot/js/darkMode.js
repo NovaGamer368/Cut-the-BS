facd4ab48853bb77f01d4dba3fd33244aff122a5
Brennan Phifer <bman5803@gmail.com>
2023-03-02 17:19:16 -0700

    Darkmode works! This makes darkmode work throughout all pages

--- Cut the BS/wwwroot/js/darkMode.js
+++ Cut the BS/wwwroot/js/darkMode.js
@@ -1,5 +1,34 @@
-﻿function myFunction() {
+﻿var darkmode = localStorage.getItem('darkMode');
+console.log(darkmode);
+ 
+function execute() {
+    if (darkmode == 'true') {
+        var element = document.body;
+        element.classList.toggle("dark");
+        console.log("Darkmode set!")
+    }
+    else {
+        console.log("Darkmode not set")
+    }
+};
+
+function myFunction() {
     var element = document.body;
     element.classList.toggle("dark");
 
+    darkmode = localStorage.getItem('darkMode')
+    switch (darkmode) {
+        case 'true':
+            console.log('Darkmode is on | ' + localStorage.getItem('darkMode'));
+            localStorage.setItem('darkMode', 'false')
+            break;
+        case 'false':
+            console.log('Darkmode is off | ' + localStorage.getItem('darkMode'));
+            localStorage.setItem('darkMode', 'true')
+            break;
+        default:
+            console.log(localStorage.getItem('darkMode'));
+            localStorage.setItem('darkMode', 'true')
+            break;
+    }
 }
